a8b0c77355431cfb000fb411ae7a3bd46c7d1798
5no0p <mmoaaa1989@hotmail.com>
2021-09-26 10:22:11 +0200

    add REACT_APP_NODEENV to api url

--- src/services/api.urls.js
+++ src/services/api.urls.js
@@ -2,7 +2,7 @@
 
 
 //export const TicketScanerApiBaseUrl = process.env.REACT_APP_BaseUrl 
-export const TicketScanerApiBaseUrl = process.env.NODE_ENV === 'production'?process.env.REACT_APP_BaseUrl:'http://localhost:8000/api/' 
+export const TicketScanerApiBaseUrl = process.env.REACT_APP_NODEENV === 'production'?process.env.REACT_APP_BaseUrl:'http://localhost:8000/api/' 
 
 export const TicketScanerApiV1BaseUrl =
   `${TicketScanerApiBaseUrl}v1/`;
