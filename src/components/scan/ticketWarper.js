a6c32dc3eb6788ca03104709268b61f1afae3446
5no0p <mmoaaa1989@hotmail.com>
2021-09-26 23:20:55 +0200

    edit style felx to validity
    add justify-content-center to validity

--- src/components/scan/ticketWarper.js
+++ src/components/scan/ticketWarper.js
@@ -59,7 +59,7 @@ console.log("histoooo: ",history)
       {ticketData && 
     <div style={{margin: "10vh 1vw"}}>
       <div className={`${ticketData.ticket.validity===true?"bg-success":"bg-danger"} h-auto w-100 d-flex justify-content-center`}>
-        {ticketData.validity?"valid":"expire"}
+        {ticketData.validity?"valid":"expired"}
       </div>
       <div className="row">
         <div className="col-9">{/* event name */}
