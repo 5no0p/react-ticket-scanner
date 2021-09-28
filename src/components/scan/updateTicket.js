5f889bd0d6cdeacbe8b30e15e4c4bfccd249b4cd
5no0p <mmoaaa1989@hotmail.com>
2021-09-28 14:00:12 +0200

    make update ticket component

--- src/components/scan/updateTicket.js
+++ src/components/scan/updateTicket.js
@@ -5,9 +5,7 @@ import {UpdateTicket} from '../../features/ticket/ticket.api'
 export const CheckTicket = (data) => {
     const mutation = useMutation(usernfo => UpdateTicket(usernfo))
     console.log("start update")
-    if(localStorage.getItem('token')){
-    console.log("start update 2")
     mutation.mutate(data)
-    }
+    
 }
   
