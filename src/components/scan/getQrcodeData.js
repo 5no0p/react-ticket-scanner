d3435c706e6d137b4343d0392265a01f07927513
5no0p <mmoaaa1989@hotmail.com>
2021-10-04 04:03:55 +0200

    TicketByIdQuery

--- src/components/scan/getQrcodeData.js
+++ src/components/scan/getQrcodeData.js
@@ -25,7 +25,7 @@ export function GetQrcodeData(ticketQrcode){
         // get the token
           const token = localStorage.getItem('token')??""
         //       send api with ticket uuiid
-          const {data,isFetching,isSuccess,isError,isLoading,error,status,isFetched} = queryClient.fetchQuery(['ticket',ticketQrcode],()=>GetTicketById(ticketQrcode))
+          const {data,isFetching,isSuccess,isError,isLoading,error,status,isFetched} = TicketByIdQuery(ticketQrcode)
           const getData = data?('status' in data)?data.data:data:data
           
 
